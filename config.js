0bf131067c1f204b079b1dcf835a503461fda3b8
LORENZO-BOTJJ <113599551+LORENZO-BOTJJ@users.noreply.github.com>
2023-04-05 19:00:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,105 @@
+/*[[Ini untuk setting Bot
+Untuk gambar background menu 
+ada 
+di plugins menu
+untuk lainya saran jangan 
+di ubah bre nanti eror]]*/
+
+/*global owner*/
+global.owner = ['6281217158010']  
+global.mods = ['6281217158010'] 
+global.prems = ['6281217158010']
+/*Nomor Owner & payment*/
+global.nameowner = 'RenzoBot'
+global.numberowner = '6281217158010' 
+global.mail = 'lorenzoelado@my.id' //enter your email 🗿
+global.dana = '6281217158010'
+global.pulsa = '6281217158010'
+global.gopay = '6281217158010'
+/*Nama Bot & Grouplink*/
+global.namebot = 'RTXZY-MD'
+global.gc = 'https://chat.whatsapp.com/D6BZVRSymWaFU28t2lxaO0'
+global.web = 'http://lorenzogame.epizy.com'
+global.instagram = 'https://instagram.com/lorenzoelado123'
+/*global wm*/
+global.lolkey = 'rey2k22'
+global.zenzkey = 'ZoeBot'
+global.wm = '© Zoe-MD'
+global.watermark = wm
+global.wm2 = '⫹⫺ Zoe-MD'
+global.wm3 = '© Zoe-MD'
+global.wm4 = '© BOT Zoe'
+global.fla = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text='
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made with'
+global.author = 'Bot WhatsApp\nR-TXZY-MD'
+/*Rest api setting*/
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  tio: 'https://api.botcahx.biz.id',
+  popcat: 'https://api.popcat.xyz',
+  rey: 'https://sekha.me'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://sekha.me': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://pencarikode.xyz': 'pais', 
+  'https://zekais.com': 'apikeymu',
+  'https://api.botcahx.biz.id': 'Admin', //daftar sendiri kalo abis
+  'https://api.lolhuman.xyz': 'Deffbotz',
+}
+
+/*Setting Rpg*/
+global.multiplier = 45
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+/*Jangan merubah bagian ini!*/
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
